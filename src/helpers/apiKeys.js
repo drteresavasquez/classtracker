9f0f63732c31c0ec60d838e0c2eeb295bf40ca02
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-06-09 15:53:15 -0500

    set up proxy

--- src/helpers/apiKeys.js
+++ src/helpers/apiKeys.js
@@ -8,7 +8,8 @@ const apiKeys = {
         messagingSenderId: process.env.REACT_APP_PT_MESSAGING_SENDER_ID,
         appId: process.env.REACT_APP_PT_APP_ID,
         measurementId: process.env.REACT_APP_PT_MEASUREMENT_ID
-    }
+    },
+    githubProxyUrl: 'https://classtracker-github-proxy.herokuapp.com'
   };
   
   export default apiKeys;
